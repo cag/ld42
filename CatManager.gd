0a500fc1503aa794a1d16e828f75f5065a2deea0
Alan Lu <alanlu1023@gmail.com>
2018-08-10 20:59:11 -0500

    Spawn some cats randomly

--- CatManager.gd
+++ CatManager.gd
@@ -1,16 +1,46 @@
 extends Node
 
-# class member variables go here, for example:
 var protocat = preload("res://Cat.tscn")
-# var b = "textvar"
+var navpoly
+var navbounds
+
+func calc_bounds(vertices):
+	var min_x = INF
+	var min_y = INF
+	var max_x = -INF
+	var max_y = -INF
+
+	for v in vertices:
+		min_x = min(min_x, v.x)
+		min_y = min(min_y, v.y)
+		max_x = max(max_x, v.x)
+		max_y = max(max_y, v.y)
+
+	return PoolVector2Array([
+		Vector2(min_x, min_y),
+		Vector2(max_x, max_y),
+	])
+
+func rand_pt_in_navpoly():
+	assert(navbounds && navpoly)
+	var minv = navbounds[0]
+	var maxv = navbounds[1]
+	var w = maxv.x - minv.x
+	var h = maxv.y - minv.y
+	while true:
+		var pt = Vector2(randf() * w, randf() * h)
+		if pt == $Navigation2D.get_closest_point(pt):
+			# point is in the navigation polygon
+			return pt
+
+func spawn_cat_somewhere():
+	var cat = protocat.instance()
+	cat.global_position = rand_pt_in_navpoly()
+	add_child(cat)
 
 func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	var cat = protocat.instance()
-	add_child(cat)
-	print(cat)
-
+	navpoly = $Navigation2D/NavigationPolygonInstance.navpoly
+	navbounds = calc_bounds(self.navpoly.get_vertices())
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
